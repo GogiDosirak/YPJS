b9f3f51cd48c0e3c35acea5caac5425e2d8bd4be
Seongjun Yang <yyjjmm2003@naver.com>
2024-06-15 05:43:20 +0900

    회원가입, 로그인 Validation + Dto 패키지 생성 후 정리

--- src/main/java/ypjs/project/controller/LoginForm.java
+++
@@ -1,9 +0,0 @@
-package ypjs.project.controller;
-
-import lombok.Data;
-
-@Data
-public class LoginForm {
-    private String accountId;
-    private String password;
-}
--- src/main/java/ypjs/project/controller/MemberApiController.java
+++ src/main/java/ypjs/project/controller/MemberApiController.java
@@ -12,6 +12,7 @@ import ypjs.project.domain.Address;
 import ypjs.project.domain.Member;
 import ypjs.project.domain.Role;
 import ypjs.project.domain.Status;
+import ypjs.project.dto.memberdto.MemberDto;
 import ypjs.project.service.MemberService;
 
 import java.sql.Date;
@@ -27,15 +28,15 @@ public class MemberApiController {
 
     // 멤버 가입
     @PostMapping("/ypjs/member/join")
-    public CreateMemberResponse join(@RequestBody @Valid CreateMemberRequest request) {
+    public MemberDto.CreateMemberResponse join(@RequestBody @Valid MemberDto.CreateMemberRequest request) {
         Member member = getJoinMemberData(request);
 
         Long memberId = memberService.join(member);
-        return new CreateMemberResponse(memberId);
+        return new MemberDto.CreateMemberResponse(memberId);
 
     }
 
-    private Member getJoinMemberData(CreateMemberRequest request) {
+    private Member getJoinMemberData(MemberDto.CreateMemberRequest request) {
         Member member = new Member();
         member = member.createMember(request.getAccountId(), request.getPassword(), request.getNickname(), request.getName(), request.getBirth(), request.getGender(),
                 request.getAddress(), request.getAddressDetail(), request.getZipcode(), request.getEmail(), request.getPhonenumber(), request.getJoinDate());
@@ -44,11 +45,11 @@ public class MemberApiController {
 
     // 멤버 수정
     @PutMapping("/ypjs/member/update/{memberId}")
-    public UpdateMemberResponse updateMember (@PathVariable("memberId") Long memberId,
-                                              @RequestBody @Valid UpdateMemberRequest request)  {
+    public MemberDto.UpdateMemberResponse updateMember (@PathVariable("memberId") Long memberId,
+                                                        @RequestBody @Valid MemberDto.UpdateMemberRequest request)  {
         memberService.update(memberId, request.getAccountId(), request.getPassword(), request.getNickname(), request.getName());
         Member member = memberService.findOne(memberId);
-        return new UpdateMemberResponse(member.getMemberId(), member.getAccountId());
+        return new MemberDto.UpdateMemberResponse(member.getMemberId(), member.getAccountId());
     }
 
     // 멤버 전체 조회 (관리자 페이지)
@@ -56,8 +57,8 @@ public class MemberApiController {
     public Result members() {
         List<Member> findMembers = memberService.findAll();
         // 찾은 정보를 Dto로 변환 후, Data로 감쌈
-        List<MemberDto> result = findMembers.stream()
-                .map(m -> new MemberDto(m.getMemberId(),m.getAccountId(),m.getNickname()))
+        List<MemberDto.MemberApiDto> result = findMembers.stream()
+                .map(m -> new MemberDto.MemberApiDto(m.getMemberId(),m.getAccountId(),m.getNickname()))
                 .collect(Collectors.toList());
         return new Result(result);
     }
@@ -65,103 +66,33 @@ public class MemberApiController {
     // 마이페이지
     @GetMapping("/ypjs/member/mypage/{memberId}")
     public Result mypage(@PathVariable("memberId") Long memberId) {
-        MypageDto result = getMypageData(memberId);
+        MemberDto.MypageDto result = getMypageData(memberId);
         return new Result(result);
 
     }
 
     // 회원탈퇴
     @PutMapping("/ypjs/member/withdrawal/{memberId}")
-    public WithdrawalMemberResponse withdraw(@PathVariable("memberId") Long memberId) {
+    public MemberDto.WithdrawalMemberResponse withdraw(@PathVariable("memberId") Long memberId) {
         memberService.witrdraw(memberId);
-        return new WithdrawalMemberResponse(memberId);
+        return new MemberDto.WithdrawalMemberResponse(memberId);
     }
 
     // member를 Dto로 감싸주는 역할
-    private MypageDto getMypageData(Long memberId) {
+    private MemberDto.MypageDto getMypageData(Long memberId) {
         Member member = memberService.findOne(memberId);
-        MypageDto result = new MypageDto(member.getAccountId(),member.getPassword(),member.getNickname(), member.getName(), member.getEmail(),member.getAddress(),
+        MemberDto.MypageDto result = new MemberDto.MypageDto(member.getAccountId(),member.getPassword(),member.getNickname(), member.getName(), member.getEmail(),member.getAddress(),
                 member.getPhonenumber(), member.getJoinDate(), member.getRole());
         return result;
     }
 
-
-    @Data
-        static class CreateMemberRequest {
-            @NotEmpty
-            private String accountId;
-            private String password;
-            private String nickname;
-            private String name;
-            private Date birth;
-            private String gender;
-            private String address;
-            private String addressDetail;
-            private String zipcode;
-            private String email;
-            private String phonenumber;
-            private LocalDateTime joinDate;
-        }
-
-        @Data
-        static class CreateMemberResponse {
-            private Long memberId;
-
-            public CreateMemberResponse(Long memberId) {
-                this.memberId = memberId;
-            }
-        }
-
-        @Data
-    static class UpdateMemberRequest {
-        private Long memberId;
-        private String accountId;
-        private String password;
-        private String nickname;
-        private String name;
-        }
-
-        @Data
-        @AllArgsConstructor
-    static class UpdateMemberResponse {
-        private Long memberId;
-        private String accountId;
-        }
-
-        @Data
-        @AllArgsConstructor
-        static class Result<T> {
-            private T data;
-        }
-
-        @Data
-    @AllArgsConstructor
-    static class MemberDto {
-        private Long memberId;
-        private String accountId;
-        private String nickname;
-        }
-
-        @Data
-    @AllArgsConstructor
-    static class MypageDto {
-        private String accountId;
-        private String password;
-        private String nickname;
-        private String name;
-        private String email;
-        private Address address;
-        private String phonenumber;
-        private LocalDateTime joinDate;
-        private Role role;
-        }
-
-
     @Data
     @AllArgsConstructor
-    static class WithdrawalMemberResponse {
-        private Long memberId;
+    static class Result<T> {
+        private T data;
     }
+
+
 
 }
 
