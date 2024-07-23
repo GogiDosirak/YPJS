26621cf01d97c079bea17326eb6c982d6a19328e
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2024-07-24 07:08:28 +0900

    회원가입 변경 1) 아이디 중복확인 추가 - member.js 수정 - join.html 수정 2) 마이페이지 비밀번호 확인 추가 - mypage.html 수정
    이메일 중복확인 구현중..
    - MemberApiController
    - MemberRepository
    - MemberService 관련 로직 추가
    - join.html ajax 통신에 문제가 있는 것 같음

--- src/main/java/ypjs/project/controller/MemberApiController.java
+++ src/main/java/ypjs/project/controller/MemberApiController.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ypjs.project.domain.Member;
@@ -44,10 +47,40 @@ public class MemberApiController {
     // 이메일 인증
     @PostMapping("/login/mailConfirm")
     @ResponseBody
-    String mailConfirm(@RequestParam("email") String email) throws Exception {
+    public String mailConfirm(@RequestParam("email") String email) throws Exception {
         String code = registerMail.sendSimpleMessage(email);
         System.out.println("인증코드 : " + code);
         return code;
+    }
+
+    // 아이디 중복 확인
+    @PostMapping("api/ypjs/member/validateDuplication")
+    public ResponseEntity<String> validateDuplication(@RequestBody String username) {
+        try {
+            memberService.validateButton(username);
+            return ResponseEntity.ok("사용 가능한 아이디입니다.");
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("중복된 아이디입니다.");
+        }
+    }
+
+    // 이메일 중복 확인
+    @PostMapping("/api/ypjs/member/validateEmail")
+    public ResponseEntity<String> validateEmail(@RequestBody EmailRequest emailRequest) {
+        System.out.println("Received email: " + emailRequest.getEmail());
+        String email = emailRequest.getEmail();
+        try {
+            memberService.validateEmail(email);
+            return ResponseEntity.ok("사용 가능한 이메일입니다.");
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("중복된 이메일입니다");
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class EmailRequest {
+        private String email;
     }
 
 
